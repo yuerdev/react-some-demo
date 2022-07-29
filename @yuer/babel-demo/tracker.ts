d487c35c0fcbcecb5a99f89aabe32f1a22654390
yuerdev <525168624@qq.com>
2022-07-29 16:25:57 +0800

    fix: babel

---
+++ @yuer/babel-demo/tracker.ts
@@ -0,0 +1,44 @@
+import type { NodePath } from '@babel/core';
+import { addDefault } from '@babel/helper-module-imports';
+import { Program } from '@babel/types';
+
+export default (api: any, options: any) => {
+    return {
+        visitor: {
+            Program: {
+                enter(path: NodePath<Program>, state) {
+                    path.traverse({
+                        ImportDeclaration(curPath) {
+                            const requirePath = curPath.get('source').node.value;
+                            if (requirePath == options.trackerPath) { // 已经引入
+                                const specifierPath = curPath.get('specifiers.0') as any;
+                                if (specifierPath.isImportSpecifier()) {
+                                    state.trackerImportId = specifierPath.toString();
+                                } else if (specifierPath.isImportNamespaceSpecifier()) {
+                                    state.trackerImportId = specifierPath.get('local').toString();
+                                }
+                                path.stop();//找到了，终止遍历
+                            }
+                        }
+                    });
+                    if (!state.trackerImportId) {
+                        state.trackerImportId = addDefault(path, 'tracker', {
+                            nameHint: path.scope.generateUidIdentifier('tracker')
+                        }).name;// tracker的模拟id
+                        console.info(state.trackerImportId, '==')
+                        state.trackerAST = api.template.statement(`${state.trackerImportId}()`)()//
+                    }
+                }
+            },
+            'ClassMethod|ArrowFunctionExpression|FunctionExpression|FunctionDeclaration'(path, state) {
+                const bodyPath = path.get('body');
+                if (bodyPath.isBlockStatement()) {
+                    bodyPath.node.body.unshift(state.trackerAST);
+                } else {
+                    const ast = api.template.statement(`{${state.trackerImportId}();return PREV_BODY;}`)({ PREV_BODY: bodyPath.node });
+                    bodyPath.replaceWith(ast);
+                }
+            }
+        }
+    }
+}
